e94ee1fdba2b9c4112d020071f24779251bab7e6
Wstld <Wstld@users.noreply.github.com>
2020-09-16 16:13:54 +0200

    Early stage of employee class.

---
+++ app/src/main/java/com/example/runner/Employee.kt
@@ -0,0 +1,5 @@
+package com.example.runner
+
+class Employee(val id:Int,val name:String,var competence:MutableMap<String,Boolean> = mutableMapOf("Bm" to false, "Ch" to false,"Chs" to false,"Rdl" to false, "Rd" to false ))  {
+
+}
